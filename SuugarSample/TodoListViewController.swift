d858f828f6b6095edbff7b4b7a445d5f023f10f3
thedoritos <thedoritos@gmail.com>
2019-03-24 22:53:26 +0900

    Show empty view

---
+++ SuugarSample/TodoListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListViewController.swift
+//  SuugarSample
+//
+//  Created by thedoritos on 2019/03/24.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "TODO:"
+        self.view.backgroundColor = .white
+    }
+}
