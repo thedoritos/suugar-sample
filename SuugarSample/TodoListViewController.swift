dcb8b6eb7554c5cbaedc844e729ecb3f857858fc
thedoritos <thedoritos@gmail.com>
2019-03-26 00:54:52 +0900

    Show simple table

--- SuugarSample/TodoListViewController.swift
+++ SuugarSample/TodoListViewController.swift
@@ -11,6 +11,16 @@ import MaterialComponents
 import Stevia
 
 class TodoListViewController: UIViewController {
+    private let todoList: [String] = [
+        "Shirobako",
+        "Tari Tari",
+        "Hanasaku Iroha",
+        "Angel Beats!",
+        "The Eccentric Family",
+        "True Tears",
+        "Glasslip"
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +28,15 @@ class TodoListViewController: UIViewController {
 
         ui {
             $0.backgroundColor = .white
+
+            $0.table {
+                $0.freeFrame()
+                $0.fillContainer()
+
+                $0.estimatedRowHeight = UITableView.automaticDimension
+                $0.register(UITableViewCell.self, forCellReuseIdentifier: "Todo")
+                $0.dataSource = self
+            }
 
             $0.composite(of: MDCFloatingButton.self) {
                 let safeArea = self.view.safeAreaLayoutGuide
@@ -29,5 +48,20 @@ class TodoListViewController: UIViewController {
                 $0.setImage(UIImage(named: "baseline_add_white_24pt"), for: .normal)
             }
         }
+    }
+}
+
+extension TodoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Todo", for: indexPath)
+
+        let todo = todoList[indexPath.row]
+        cell.textLabel?.text = todo
+
+        return cell
     }
 }
