e68a314eb9332d9a2fb5094d77a1bbf1c174db38
thedoritos <thedoritos@gmail.com>
2019-03-28 20:36:59 +0900

    Add due date input

--- SuugarSample/TodoFormViewController.swift
+++ SuugarSample/TodoFormViewController.swift
@@ -9,13 +9,19 @@ import UIKit
 import Suugar
 import Stevia
 import MaterialComponents
+import RxSwift
+import RxCocoa
 
 class TodoFormViewController: UIViewController {
     private weak var titleInput: MDCTextField!
+    private weak var dueDateInput: MDCTextField!
     private weak var descriptionInput: MDCMultilineTextField!
 
     private var titleController: MDCTextInputControllerOutlined!
+    private var dueDateController: MDCTextInputControllerOutlined!
     private var descriptionController: MDCTextInputControllerOutlinedTextArea!
+
+    private let dispoaseBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +47,30 @@ class TodoFormViewController: UIViewController {
                             $0.placeholder = "Title"
                         }
 
+                        dueDateInput = $0.composite() {
+                            $0.placeholder = "Due Date"
+
+                            let picker = UIDatePicker()
+                            picker.datePickerMode = .date
+
+                            let formatter = DateFormatter()
+                            formatter.dateFormat = "yyyy/MM/dd"
+                            picker.rx.date
+                                .map { formatter.string(from: $0) }
+                                .bind(to: $0.rx.text)
+                                .disposed(by: dispoaseBag)
+
+                            $0.inputView = picker
+
+                            let pickerToolbar = UIToolbar()
+                            pickerToolbar.sizeToFit()
+                            pickerToolbar.items = [
+                                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+                                UIBarButtonItem(title: "Done", style: .done, target: $0, action: #selector($0.resignFirstResponder))
+                            ]
+                            $0.inputAccessoryView = pickerToolbar
+                        }
+
                         descriptionInput = $0.composite() {
                             $0.placeholder = "Note"
                         }
@@ -50,6 +80,7 @@ class TodoFormViewController: UIViewController {
         }
 
         titleController = MDCTextInputControllerOutlined(textInput: titleInput)
+        dueDateController = MDCTextInputControllerOutlined(textInput: dueDateInput)
         descriptionController = MDCTextInputControllerOutlinedTextArea(textInput: descriptionInput)
     }
 }
